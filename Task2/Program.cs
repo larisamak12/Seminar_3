8af1717bafd4aa38cf469ab2db7787caa35a6bf9
Larisa <larisamak12@mail.ru>
2022-10-26 14:11:17 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите число");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int z2 = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+
+Console.WriteLine(d);
